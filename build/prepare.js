dc51ac5b4d42dcf52969d9fc4668fb6c045e8cc0
Helio Frota <00hf11@gmail.com>
2017-05-05 17:42:40 -0300

    build: working with all and with specific OS and VERSION

--- build/prepare.js
+++ build/prepare.js
@@ -12,30 +12,32 @@ const fs = require('fs');
 const _ = require('underscore');
 const releases = require('../releases.json');
 
-// OS and VERSION checks.
-// const validOSVersions = process.env.VALID_OS.split(' ');
+const validOSVersions = process.env.VALID_OS.split(' ');
 
+// OS and VERSION checks.
 // if (validOSVersions.find(os => os === process.env.OS)) {
+for (const OS of validOSVersions) {
   fs.mkdir(`target`, (err) => {
     if (err && err.code !== 'EEXIST') return console.log(err);
-    processFiles(`${process.env.OS}/Dockerfile.${process.env.OS}`, releases);
-    processFiles(`${process.env.OS}/Dockerfile.${process.env.OS}.onbuild`, releases);
+    processFiles(`${OS}/Dockerfile.${OS}`, releases, OS);
+    processFiles(`${OS}/Dockerfile.${OS}.onbuild`, releases, OS);
   });
+}
 // }
 
-function processFiles (file, releases) {
+function processFiles (file, releases, OS) {
   fs.readFile(file, 'utf-8', (err, txt) => {
     _.each(_.keys(releases), (version) => {
       const output = transform(txt, releases[version]);
       const osDir = path.join('target', releases[version].version);
       fs.mkdir(osDir, (err) => {
         // Try to create a directory for each version.
-        const targetDir = path.join(`target/${releases[version].version}`, process.env.OS);
+        const targetDir = path.join(`target/${releases[version].version}`, OS);
         fs.mkdir(targetDir, (err) => {
           // Reject with any error other than EEXIST
           if (err && err.code !== 'EEXIST') throw(err);
           // Write the dockerfile
-          file = file.replace(process.env.OS + '/' , '');
+          file = file.replace(OS + '/' , '');
           const dockerFilePath = path.join(targetDir, file);
           console.log(`Writing ${dockerFilePath}`);
           fs.writeFileSync(dockerFilePath, output);
